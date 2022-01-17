eff538aca6731a26ae869145b0f76f2932c984c0
RomanovAnton <rad250495@yandex.ru>
2022-01-17 22:59:43 +0700

    spri4_rev21

--- index.js
+++ index.js
@@ -2,31 +2,27 @@ const openPopupButton = document.querySelector('.profile__edit-Button');
 const popup = document.querySelector('.popup');
 const closePopupButton = document.querySelector('.popup__button-close');
 
-let profileName = document.querySelector('.popup__user-name')
-let profileJob = document.querySelector('.popup__user-job')
+let PopupNameInput = document.querySelector('.popup__profile-name');
+let PopupJobInput = document.querySelector('.popup__profile-job');
+let formElement = document.querySelector('.popup__form');
+
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
 
 function openPopup() {
   popup.classList.add('popup_opened')
-  profileJob.value = document.querySelector('.profile__job').textContent;
-  profileName.value = document.querySelector('.profile__name').textContent;
+  PopupNameInput.value = document.querySelector('.profile__name').textContent;
+  PopupJobInput.value = document.querySelector('.profile__job').textContent;
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened')
 }
 
-openPopupButton.addEventListener('click', openPopup);
-closePopupButton.addEventListener('click', closePopup);
-
-
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__user-name')
-let jobInput = document.querySelector('.popup__user-job')
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  document.querySelector('.profile__job').textContent = jobInput.value;
-  document.querySelector('.profile__name').textContent = nameInput.value;
+  document.querySelector('.profile__job').textContent = PopupJobInput.value;
+  document.querySelector('.profile__name').textContent = PopupNameInput.value;
   closePopup()
 }
 
