cefd82a87dd19b0171fbaaa85ea7baec8d72d592
RomanovAnton <rad250495@yandex.ru>
2022-03-19 23:15:30 +0700

    PR8_rev2

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,10 @@ import {
   imagePopup,
   editButton,
   addButton,
-  popupEditNameInput,
-  popupEditJobInput,
   profileName,
-  profileJob
-
+  profileJob,
+  editform,
+  addform
 } from '../utils/constants.js'
 
 import { Card } from '../components/Card.js'
@@ -44,14 +43,10 @@ const cards = new Section({
 cards.renderer();
 
 
-
 editButton.addEventListener('click', () => {
   const edit = new Popup(editPopup)
   edit.open()
-  test.getUserInfo()
-  // popupEditNameInput.value = test.getUserInfo().profileName
-  // popupEditJobInput.value = test.getUserInfo().profileJob
-
+  userInfo.getUserInfo()
   edit.setEventListeners()
 });
 
@@ -61,22 +56,16 @@ addButton.addEventListener('click', () => {
   add.setEventListeners()
 });
 
-
-
-
-
-const editForm = new PopupWithForm({
+const popupEditForm = new PopupWithForm({
   popup: editPopup,
   handleSubmitForm: (formData) => {
-    
-    test.setUserInfo(formData)
-    
+    userInfo.setUserInfo(formData)
   }
 })
 
-editForm.setEventListeners()
+popupEditForm.setEventListeners()
 
-const addForm = new PopupWithForm({
+const popupAddForm = new PopupWithForm({
   popup: addPopup,
   handleSubmitForm: (formData) => {
     const newCard = new Card({
@@ -92,65 +81,16 @@ const addForm = new PopupWithForm({
     cards.addItem(newCardElement);
   }
 })
-
-addForm.setEventListeners()
-
-
-
-
-const test = new UserInfo(profileName, profileJob)
+popupAddForm.setEventListeners()
+const userInfo = new UserInfo(profileName, profileJob)
 
 
 
+const editProfileValidator = new FormValidator(config, editform)
+const addCardValidator = new FormValidator(config, addform)
+editProfileValidator.enableValidation()
+addCardValidator.enableValidation()
 
-
-// РАБОЧИЙ
-// const editForm = new PopupWithForm({
-//   popup: editPopup,
-//   handleSubmitForm: (formData) => {
-//     profileName.textContent = formData.profileName
-//     profileJob.textContent = formData.profileJob
-//   }
-// })
-
-
-
-
-
-
-
-
-
-
-// function handleProfileFormSubmit(evt) {
-//   evt.preventDefault();
-//   profileName.textContent = popupEditNameInput.value;
-//   profileJob.textContent = popupEditJobInput.value;
-//   closePopup(editPopup);
-// }
-
-// function handleAddFormSubmit(evt) {
-//   evt.preventDefault();
-//   createNewCard();
-//   addform.reset();
-//   closePopup(addPopup);
-// }
-
-
-
-
-// editform.addEventListener('submit', handleProfileFormSubmit);
-// addform.addEventListener('submit', handleAddFormSubmit);
-
-
-
-
-
-// const editProfileValidator = new FormValidator(config, editform)
-// const addCardValidator = new FormValidator(config, addform)
-// editProfileValidator.enableValidation()
-// addCardValidator.enableValidation()
-// renderElements()
 
 
 
